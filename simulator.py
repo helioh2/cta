c67ccf280643bf776b97ecb6d1a922578bc0d559
helioh2 <heliohenrique3@gmail.com>
2017-04-18 14:14:57 -0300

    Desenvolvendo inserção e movimento de carros

--- simulator.py
+++ simulator.py
@@ -3,17 +3,42 @@
 
 from collections import deque
 
-def genStreetId():
+COUNT_STREETS = 10
+
+def genCarId():
     count = 0
     while True:
         yield count
         count += 1
+        
+def genStreetId():
+    for i in range(COUNT_STREETS):
+        yield i
 
 DIRECTIONS = ["H", "V"]
 SENSES = ["->", "<-"]
 
 class FullLaneException(Exception):
     pass
+
+
+class Car:
+    
+    def __init__(self, street):
+        self.street = street
+        self.id = genCarId()
+        self.totalTimeInSim = 0
+        self.block = None
+        
+    def setBlock(self, block):
+        self.block = block
+    
+    def setLane(self, lane):
+        self.lane = lane
+    
+    def move(self):
+        if self.block.getLane(self.lane).index() == self.block.getLength()-1:
+            
 
 class Block:
     
@@ -28,6 +53,8 @@ class Block:
         if self.lanes[lane] == self.length:
             raise FullLaneException("Lane "+str(lane)+" is full.")
         self.lanes[lane].append(car)
+        car.setBlock(self)
+        car.setLane(lane)
          
             
 
@@ -40,9 +67,9 @@ class Street:
         self.direction = direction
         self.sense = self.id % 2
         
-        self.innerBlocks = [Block(self) for i in range(countBlocks-2)]
-        self.entryBlock = Block(self)
-        self.exitBlock = Block(self)
+        self.blocks = [Block(self) for i in range(countBlocks)]
+        self.entryBlock = self.blocks[0]
+        self.exitBlock = self.blocks[-1]
         
     def getId(self):
         return self.id    
@@ -50,4 +77,18 @@ class Street:
     def genBlockId(self):
         for i in range(self.countBlocks):
             yield i
+            
+    def getEntryBlock(self):
+        return self.entryBlock
+    
+    def newCar(self, lane):
+        car = Car(self)
+        try:
+            self.entryBlock.addCar(car, lane)
+        except FullLaneException as e:
+            print(e)
+            ## Put on log
+            
+            
+    
             
